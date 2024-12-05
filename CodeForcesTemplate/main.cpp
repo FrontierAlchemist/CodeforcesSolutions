1320cc65412d92566da0367bb4c1363ad5aa5a1c
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2024-12-05 13:37:13 +0400

    Remove competitive constructions from common template code

--- CodeForcesTemplate/main.cpp
+++ CodeForcesTemplate/main.cpp
@@ -1,26 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 
-#include <algorithm>
-#include <cmath>
 #include <iostream>
-#include <iomanip>
-#include <map>
-#include <queue>
-#include <set>
-#include <stack>
-#include <string>
-#include <utility>
-#include <vector>
 
 using namespace std;
-
-#define READV(v) for (int i = 0; i < v.size(); ++i) { cin >> v[i]; }
-#define WRITEV(v) for (int i = 0; i < v.size(); ++i) { cout << v[i] << ' '; } cout << '\n';
-
-typedef unsigned long long ull;
-typedef long long ll;
-typedef vector<int> vi;
-typedef pair<int, int> pii;
 
 void RedirectIOStreams() {
 	auto fin = freopen("input.txt", "r", stdin);
