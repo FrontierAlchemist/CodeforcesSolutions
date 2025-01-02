dd5c7c3d4fc0766a2eff26bfe92b6965e55b3553
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-02 19:14:21 +0400

    Solve 'Segment with Small Sum' problem

---
+++ ProblemsSolutions/ITMO/TwoPointerTechnique/segment_with_small_sum.cs
@@ -0,0 +1,155 @@
+// https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+
+using System;
+using System.IO;
+
+public interface IInputReader
+{
+	string ReadLine();
+}
+
+public interface IOutputWriter
+{
+	void Write(string value);
+	void WriteLine(string value);
+}
+
+public class ConsoleReader : IInputReader
+{
+	public string ReadLine()
+		=> Console.ReadLine() ?? throw new FormatException("Error on trying read line from console.");
+}
+
+public class ConsoleWriter : IOutputWriter
+{
+	public void Write(string value) => Console.Write(value);
+
+	public void WriteLine(string value) => Console.WriteLine(value);
+}
+
+public abstract class FileHolder
+{
+	private const string RelativePathToFiles = "..\\..\\..\\";
+
+	protected readonly string fileName;
+
+	protected virtual string PathToFile => Path.Combine(RelativePathToFiles, fileName);
+
+	public FileHolder(string fileName)
+	{
+		this.fileName = fileName;
+	}
+
+	public abstract void CloseFile();
+}
+
+public class FileReader : FileHolder, IInputReader
+{
+	private readonly StreamReader file;
+
+	public FileReader() : base("input.txt")
+	{
+		file = new StreamReader(PathToFile);
+	}
+
+	public string ReadLine()
+		=> file.ReadLine() ?? throw new FormatException($"Error on trying read line from file: {PathToFile}.");
+
+	public override void CloseFile() => file.Close();
+}
+
+public class FileWriter : FileHolder, IOutputWriter
+{
+	private readonly StreamWriter file;
+
+	public FileWriter() : base("output.txt")
+	{
+		file = new StreamWriter(PathToFile);
+	}
+
+	public void Write(string value) => file.Write(value);
+
+	public void WriteLine(string value) => file.WriteLine(value);
+
+	public override void CloseFile() => file.Close();
+}
+
+public class Program
+{
+	const bool IsSeveralTests = false;
+
+	static readonly IInputReader reader;
+	static readonly IOutputWriter writer;
+
+	static readonly Action? onProgramClosing;
+
+	static Program()
+	{
+		if (IsDebug()) {
+			var fileReader = new FileReader();
+			var fileWriter = new FileWriter();
+
+			onProgramClosing += fileReader.CloseFile;
+			onProgramClosing += fileWriter.CloseFile;
+
+			reader = fileReader;
+			writer = fileWriter;
+		} else {
+			reader = new ConsoleReader();
+			writer = new ConsoleWriter();
+		}
+	}
+
+	static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	static void SolveTestCase()
+	{
+		string[] valuesFromInput = reader.ReadLine().Split();
+		int arraySize = int.Parse(valuesFromInput[0]);
+		long sumLimit = long.Parse(valuesFromInput[1]);
+		valuesFromInput = reader.ReadLine().Split();
+		int[] array = new int[arraySize];
+		for (int i = 0; i < arraySize; i++) {
+			array[i] = int.Parse(valuesFromInput[i]);
+		}
+
+		long currentSum = 0L;
+		int maxSegmentLength = 0;
+		int left = 0;
+		for (int right = 0; right < arraySize; ++right) {
+			currentSum += array[right];
+			while (currentSum > sumLimit) {
+				currentSum -= array[left++];
+			}
+			maxSegmentLength = Math.Max(right - left + 1, maxSegmentLength);
+		}
+
+		writer.WriteLine(maxSegmentLength.ToString());
+	}
+
+	static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(reader.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; i++) {
+			SolveTestCase();
+		}
+	}
+
+	static void Close()
+	{
+		onProgramClosing?.Invoke();
+	}
+
+	static void Main()
+	{
+		RunTests();
+		Close();
+	}
+}
