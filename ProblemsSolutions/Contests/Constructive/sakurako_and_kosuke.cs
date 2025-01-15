a4c2aa94df9976177a73050a6c1560c7bc0b9b80
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-15 22:48:27 +0400

    Solve 'Sakurako and Kosuke' problem

---
+++ ProblemsSolutions/Contests/Constructive/sakurako_and_kosuke.cs
@@ -0,0 +1,56 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/contest/2033/problem/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static readonly StreamReader input =
+		IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+
+	private static readonly StreamWriter output =
+		IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+
+	private static void SolveProblem()
+	{
+		int finalNumber = int.Parse(input.ReadLine());
+		output.WriteLine(finalNumber % 2 == 0 ? "Sakurako" : "Kosuke");
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void CloseStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		RunTests();
+		CloseStreams();
+	}
+}
