e642da7df8c416d5d4ff107445c8b11e26694b1a
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-16 20:48:12 +0400

    Solve 'Profitable Interest Rate' problem

---
+++ ProblemsSolutions/Contests/Math/profitable_interest_rate.cs
@@ -0,0 +1,60 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/contest/2024/problem/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static readonly StreamReader input =
+		IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+
+	private static readonly StreamWriter output =
+		IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+
+	private static void SolveProblem()
+	{
+		string[] valuesFromInput = input.ReadLine().Split();
+		int coinsCount = int.Parse(valuesFromInput[0]);
+		int costOfAccount = int.Parse(valuesFromInput[1]);
+		int shouldPay = Math.Max(costOfAccount - coinsCount, 0);
+		int remainder = Math.Max(coinsCount - shouldPay, 0);
+		output.WriteLine(remainder);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void CloseStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		RunTests();
+		CloseStreams();
+	}
+}
