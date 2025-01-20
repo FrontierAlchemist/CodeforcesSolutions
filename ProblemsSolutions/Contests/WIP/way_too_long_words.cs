9fbf6e6158d85c4ed1fd4bec024c764d659a6adb
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-20 22:29:31 +0400

    Add draft of 'Way Too Long Words' problem solution

---
+++ ProblemsSolutions/Contests/WIP/way_too_long_words.cs
@@ -0,0 +1,61 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/71/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static readonly StreamReader input =
+		IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+
+	private static readonly StreamWriter output =
+		IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+
+	private static void SolveProblem()
+	{
+		string sourceString = input.ReadLine();
+		if (sourceString.Length <= 10) {
+			output.WriteLine(sourceString);
+		} else {
+			string modifiedString = $"{sourceString[0]}{sourceString.Length - 2}{sourceString[^1]}";
+			output.WriteLine(modifiedString);
+		}
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void CloseStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		RunTests();
+		CloseStreams();
+	}
+}
