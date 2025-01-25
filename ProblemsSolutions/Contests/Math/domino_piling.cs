e4c2aee2614d3d06e7f59f896a7259a82a9c187e
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-25 12:42:38 +0400

    Solve 'Domino piling' problem

---
+++ ProblemsSolutions/Contests/Math/domino_piling.cs
@@ -0,0 +1,60 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/50/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static readonly StreamReader input =
+		IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+
+	private static readonly StreamWriter output =
+		IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+
+	private static void SolveProblem()
+	{
+		string[] valuesFromInput = input.ReadLine().Split();
+		int width = int.Parse(valuesFromInput[0]);
+		int height = int.Parse(valuesFromInput[1]);
+		int square = width * height;
+		int fitPiecesCount = square / 2;
+		output.WriteLine(fitPiecesCount);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void CloseStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		RunTests();
+		CloseStreams();
+	}
+}
