64443d2d26d0a91b6f26ce2fbdd748d928d52a13
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-29 18:38:55 +0400

    Solve 'Stones to remove' problem

---
+++ ProblemsSolutions/Contests/Constructive/stones_to_remove.cs
@@ -0,0 +1,67 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/266/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		int stonesCount = int.Parse(input.ReadLine());
+		string stonesOrder = input.ReadLine();
+		int stonesToRemove = 0;
+		for (int i = 0; i < stonesCount - 1; ++i) {
+			if (stonesOrder[i] == stonesOrder[i + 1]) {
+				++stonesToRemove;
+			}
+		}
+		output.WriteLine(stonesToRemove);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
