212137a6dcb9a5858a06bbbe685b0340270d8e2d
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-29 18:33:39 +0400

    Solve 'Bear and Big Brother' problem

---
+++ ProblemsSolutions/Contests/Implementation/bear_and_big_brother.cs
@@ -0,0 +1,68 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/791/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string[] valuesFromInput = input.ReadLine().Split();
+		int limakWeight = int.Parse(valuesFromInput[0]);
+		int bobWeight = int.Parse(valuesFromInput[1]);
+		int yerasPast = 0;
+		while (limakWeight <= bobWeight) {
+			limakWeight *= 3;
+			bobWeight *= 2;
+			++yerasPast;
+		}
+		output.WriteLine(yerasPast);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
