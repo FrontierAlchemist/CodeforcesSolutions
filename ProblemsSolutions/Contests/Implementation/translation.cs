0a4a88dc116868c48061f00067b208b5e7dc442d
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-01-31 10:31:06 +0400

    Solve 'Translation' problem

---
+++ ProblemsSolutions/Contests/Implementation/translation.cs
@@ -0,0 +1,74 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/41/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string sourceString = input.ReadLine();
+		string translatedString = input.ReadLine();
+
+		if (sourceString.Length != translatedString.Length) {
+			output.WriteLine("NO");
+			return;
+		}
+		int stringLength = sourceString.Length;
+		bool isTranslatedCorrectly = true;
+		for (int i = 0; i < stringLength; ++i) {
+			if (sourceString[i] != translatedString[stringLength - 1 - i]) {
+				isTranslatedCorrectly = false;
+				break;
+			}
+		}
+		output.WriteLine(isTranslatedCorrectly ? "YES" : "NO");
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
