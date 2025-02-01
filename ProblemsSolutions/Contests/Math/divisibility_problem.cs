149fcae6e2c1e6ecbdee861f061cdfd1131bd6e8
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-01 12:21:37 +0400

    Solve 'Divisibility Problem' problem

---
+++ ProblemsSolutions/Contests/Math/divisibility_problem.cs
@@ -0,0 +1,66 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/1328/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string[] valuesFromInput = input.ReadLine().Split();
+		int a = int.Parse(valuesFromInput[0]);
+		int b = int.Parse(valuesFromInput[1]);
+		int operationsCount = 0;
+		if (a % b != 0) {
+			operationsCount = b * (a / b + 1) - a;
+		}
+		output.WriteLine(operationsCount);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
