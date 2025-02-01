c07f8886f56a59aa44ca4eb8bdfe3285589e06e2
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-01 12:13:51 +0400

    Solve 'Drinks' problem

---
+++ ProblemsSolutions/Contests/Math/drinks.cs
@@ -0,0 +1,65 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/200/B problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		int drinksCount = int.Parse(input.ReadLine());
+		int numerator = 0;
+		foreach (string valueFromInput in input.ReadLine().Split()) {
+			numerator += int.Parse(valueFromInput);
+		}
+		double volumeFraction = (double)numerator / drinksCount;
+		output.WriteLine(volumeFraction);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
