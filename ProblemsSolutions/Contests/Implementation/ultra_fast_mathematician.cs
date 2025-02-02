5aed36cb9910050a5e40727ec97775ffdc1fc36d
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-02 13:29:31 +0400

    Solve 'Ultra-fast Mathematician' problem

---
+++ ProblemsSolutions/Contests/Implementation/ultra_fast_mathematician.cs
@@ -0,0 +1,64 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/61/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string firstBinaryString = input.ReadLine();
+		string secndBinaryString = input.ReadLine();
+		for (int i = 0; i < firstBinaryString.Length; ++i) {
+			output.Write(firstBinaryString[i] == secndBinaryString[i] ? 0 : 1);
+		}
+		output.WriteLine();
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
