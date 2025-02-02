0d710373306f3cb4d985e17183c8fdb0daaa1bea
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-02 12:23:41 +0400

    Solve 'Presents' problem

---
+++ ProblemsSolutions/Contests/Implementation/presents.cs
@@ -0,0 +1,66 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/136/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		int friendsCount = int.Parse(input.ReadLine());
+		int[] presenters = new int[friendsCount];
+		string[] valuesFromInput = input.ReadLine().Split();
+		for (int i = 0; i < friendsCount; ++i) {
+			int reciver = int.Parse(valuesFromInput[i]);
+			presenters[reciver - 1] = i + 1;
+		}
+		output.WriteLine(string.Join(' ',  presenters));
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
