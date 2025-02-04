355aff4655736d28d8f47c6493cf4933da26b59a
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-04 19:21:00 +0400

    Solve 'Candies and Two Sisters' problem

---
+++ ProblemsSolutions/Contests/Math/candies_and_two_sisters.cs
@@ -0,0 +1,64 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/1335/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		int candiesCount = int.Parse(input.ReadLine());
+		int waysToDisributeCount = Math.DivRem(candiesCount, 2, out int remainder);
+		if (remainder == 0) {
+			--waysToDisributeCount;
+		}
+		output.WriteLine(waysToDisributeCount);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
