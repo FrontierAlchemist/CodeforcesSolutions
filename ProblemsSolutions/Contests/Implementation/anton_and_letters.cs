972fec0f91ba76b4cfbe32d31538848ff3ce6701
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-07 18:55:26 +0400

    Solve 'Anton and Letters' problem

---
+++ ProblemsSolutions/Contests/Implementation/anton_and_letters.cs
@@ -0,0 +1,65 @@
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/443/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string prunedSourceString = input.ReadLine()[1..^1];
+		int distinctLettersCount = 0;
+		if (prunedSourceString.Length > 0) {
+			distinctLettersCount = new HashSet<string>(prunedSourceString.Split(", ")).Count;
+		}
+		output.WriteLine(distinctLettersCount);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
