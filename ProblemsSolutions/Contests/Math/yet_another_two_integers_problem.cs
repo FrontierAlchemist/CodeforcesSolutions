480670b3107e6efa7bb9b20ad7285fbf1814dacd
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-08 13:25:07 +0400

    Solve 'Yet Another Two Integers Problem' problem

---
+++ ProblemsSolutions/Contests/Math/yet_another_two_integers_problem.cs
@@ -0,0 +1,66 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/1409/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string[] valuesFromInput = input.ReadLine().Split();
+		int a = int.Parse(valuesFromInput[0]);
+		int b = int.Parse(valuesFromInput[1]);
+		int movesCount = Math.DivRem(Math.Abs(a - b), 10, out int remainder);
+		if (remainder > 0) {
+			++movesCount;
+		}
+		output.WriteLine(movesCount);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
