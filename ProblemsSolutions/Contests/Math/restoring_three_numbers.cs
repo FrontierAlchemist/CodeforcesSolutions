e4ff1322e1ebf44037fba06339d02ca91a5d7bba
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-08 13:07:10 +0400

    Solve 'Restoring Threee Numbers' problem

---
+++ ProblemsSolutions/Contests/Math/restoring_three_numbers.cs
@@ -0,0 +1,70 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/1154/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string[] valuesFromInput = input.ReadLine().Split();
+		int[] numbers = new int[4];
+		for (int i = 0; i < 4; ++i) {
+			numbers[i] = int.Parse(valuesFromInput[i]);
+		}
+		Array.Sort(numbers);
+
+		int minNumber = numbers[3] - numbers[2];
+		int middleNumber = numbers[0] - minNumber;
+		int maxNumber = numbers[1] - minNumber;
+
+		output.WriteLine($"{minNumber} {middleNumber} {maxNumber}");
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
