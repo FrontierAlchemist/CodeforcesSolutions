34a64a175fa0ce68ec68bf2cc38e4f8de7429d6e
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-09 20:57:08 +0400

    Solve 'Skibidus and Amogu' problem

---
+++ ProblemsSolutions/Contests/Implementation/skibidus_and_amogu.cs
@@ -0,0 +1,61 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/contest/2065/problem/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string wordInSingularForm = input.ReadLine();
+		string wordInPluralForm = wordInSingularForm[0..^2] + "i";
+		output.WriteLine(wordInPluralForm);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
