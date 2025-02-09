b2a39fdad0d5c345e35dd142d2fb5aaa424569e1
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-09 20:57:56 +0400

    Solve 'Skibidus and Ohio' problem

---
+++ ProblemsSolutions/Contests/Constructive/skibidus_and_ohio.cs
@@ -0,0 +1,68 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/contest/2065/problem/B problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = true;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		string sourceString = input.ReadLine();
+		bool isModifiable = false;
+		for (int i = 0; i < sourceString.Length - 1; i++) {
+			if (sourceString[i] == sourceString[i + 1]) {
+				isModifiable = true;
+				break;
+			}
+		}
+		int resultStringLength = isModifiable ? 1 : sourceString.Length;
+		output.WriteLine(resultStringLength);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
