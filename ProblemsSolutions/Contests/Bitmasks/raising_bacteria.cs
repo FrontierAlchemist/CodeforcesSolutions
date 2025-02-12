992ba4aa485f8fa356196a447a5709b469042d51
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-12 20:06:18 +0400

    Solve 'Raising Bacteria' problem

---
+++ ProblemsSolutions/Contests/Bitmasks/raising_bacteria.cs
@@ -0,0 +1,68 @@
+#nullable disable
+
+using System;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/579/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReader input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		int bacteriesTargetNumber = int.Parse(input.ReadLine());
+		int generationsCount = 0;
+		while (bacteriesTargetNumber > 0) {
+			bool isCurrentNumberEven = (bacteriesTargetNumber & 1) == 0;
+			if (!isCurrentNumberEven) {
+				++generationsCount;
+			}
+			bacteriesTargetNumber >>= 1;
+		}
+		output.WriteLine(generationsCount);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.ReadLine()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIoStreams()
+	{
+		input = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIoStreams()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIoStreams();
+		RunTests();
+		CloseIoStreams();
+	}
+}
