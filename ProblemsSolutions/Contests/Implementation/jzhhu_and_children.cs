35438deb58cd4942ad2c6d1872537ce9f1272d64
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-15 00:23:58 +0400

    Solve 'Jzzhu and Children' problem

---
+++ ProblemsSolutions/Contests/Implementation/jzhhu_and_children.cs
@@ -0,0 +1,133 @@
+#nullable disable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/450/A problem.
+/// </summary>
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static StreamReaderWrapper input;
+	private static StreamWriter output;
+
+	private static void SolveProblem()
+	{
+		int childrensCount = input.GetInt();
+		int portionSize = input.GetInt();
+		Queue<(int desiredCandiesCount, int number)> childrensQueue = new();
+		for (int i = 1; i <= childrensCount; ++i) {
+			int desiredCandiesCount = input.GetInt();
+			childrensQueue.Enqueue((desiredCandiesCount, i));
+		}
+		while (childrensQueue.Count > 1) {
+			var child = childrensQueue.Dequeue();
+			child.desiredCandiesCount -= portionSize;
+			if (child.desiredCandiesCount > 0) {
+				childrensQueue.Enqueue(child);
+			}
+		}
+		int lastChildrenNumber = childrensQueue.Dequeue().number;
+		output.WriteLine(lastChildrenNumber);
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(input.GetString()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			SolveProblem();
+		}
+	}
+
+	private static void OpenIo()
+	{
+		var inputStream = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		input = new StreamReaderWrapper(inputStream);
+		output = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+	}
+
+	private static void CloseIo()
+	{
+		input.Close();
+		output.Close();
+	}
+
+	private static void Main()
+	{
+		OpenIo();
+		RunTests();
+		CloseIo();
+	}
+}
+
+internal class StreamReaderWrapper
+{
+	private readonly StreamReader streamReader;
+	private readonly IEnumerator<string> inputLinesEnumerator;
+
+	public StreamReaderWrapper(StreamReader streamReader)
+	{
+		this.streamReader = streamReader;
+		inputLinesEnumerator = GetInputLinesEnumerator();
+	}
+
+	public string GetString() => GetInputLine();
+
+	public int GetInt() => int.Parse(GetInputLine());
+
+	public int[] GetIntArray(int size)
+	{
+		int[] array = new int[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = GetInt();
+		}
+		return array;
+	}
+
+	public double GetDouble() => double.Parse(GetInputLine());
+
+	public double[] GetDoubleArray(int size)
+	{
+		double[] array = new double[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = GetDouble();
+		}
+		return array;
+	}
+
+	public void Close()
+	{
+		streamReader.Close();
+	}
+
+	private string GetInputLine()
+	{
+		inputLinesEnumerator.MoveNext();
+		return inputLinesEnumerator.Current;
+	}
+
+	private IEnumerator<string> GetInputLinesEnumerator()
+	{
+		while (true) {
+			string[] splitedLineFromInput = streamReader.ReadLine().Split();
+			foreach (var line in splitedLineFromInput) {
+				yield return line;
+			}
+		}
+	}
+}
