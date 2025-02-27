8a68136fd46fa69c52347cc80764d67bddd7913c
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-02-27 12:58:50 +0400

    Solve 'Free cash' problem

---
+++ ProblemsSolutions/Contests/Implementation/free_cash.cs
@@ -0,0 +1,172 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+internal class Solver
+{
+	private static StreamReaderWrapper Input => Program.Input;
+	private static StreamWriterWrapper Output => Program.Output;
+
+	/// <summary>
+	/// Solve https://codeforces.com/problemset/problem/237/A problem.
+	/// </summary>
+	public static void SolveProblem()
+	{
+		int customersCount = Input.ReadInt();
+		int currentCustomersCount = 0;
+		int lastCustomerTime = -1;
+		int neededCashesCount = 1;
+		for (int i = 0; i < customersCount; ++i) {
+			int hour = Input.ReadInt();
+			int minute = Input.ReadInt();
+			int currentTime = hour * 60 + minute;
+			if (currentTime == lastCustomerTime) {
+				++currentCustomersCount;
+			} else {
+				neededCashesCount = Math.Max(neededCashesCount, currentCustomersCount);
+				currentCustomersCount = 1;
+				lastCustomerTime = currentTime;
+			}
+		}
+		neededCashesCount = Math.Max(neededCashesCount, currentCustomersCount);
+		Output.WriteLine(neededCashesCount);
+	}
+}
+
+internal class Program
+{
+	private const bool IsSeveralTests = false;
+
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	public static StreamReaderWrapper Input { get; private set; }
+	public static StreamWriterWrapper Output { get; private set; }
+
+	private static void Main()
+	{
+		OpenIo();
+		RunTests();
+		CloseIo();
+	}
+
+	private static void OpenIo()
+	{
+		var inputStream = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		Input = new StreamReaderWrapper(inputStream);
+		var outputStream = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+		Output = new StreamWriterWrapper(outputStream);
+	}
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static void RunTests()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(Input.ReadString()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			Solver.SolveProblem();
+		}
+	}
+
+	private static void CloseIo()
+	{
+		Input.Close();
+		Output.Close();
+	}
+}
+
+internal class StreamReaderWrapper
+{
+	private readonly StreamReader streamReader;
+	private readonly IEnumerator<string> inputLinesEnumerator;
+
+	public StreamReaderWrapper(StreamReader streamReader)
+	{
+		this.streamReader = streamReader;
+		inputLinesEnumerator = GetInputLinesEnumerator();
+	}
+
+	public string ReadString() => ReadLine();
+
+	public int ReadInt() => int.Parse(ReadLine());
+
+	public int[] ReadIntArray(int size)
+	{
+		int[] array = new int[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadInt();
+		}
+		return array;
+	}
+
+	public double ReadDouble() => double.Parse(ReadLine());
+
+	public double[] ReadDoubleArray(int size)
+	{
+		double[] array = new double[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadDouble();
+		}
+		return array;
+	}
+
+	public long ReadLong() => long.Parse(ReadLine());
+
+	public long[] ReadLongArray(int size)
+	{
+		long[] array = new long[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadLong();
+		}
+		return array;
+	}
+
+	public void Close()
+	{
+		streamReader.Close();
+	}
+
+	private string ReadLine()
+	{
+		inputLinesEnumerator.MoveNext();
+		return inputLinesEnumerator.Current;
+	}
+
+	private IEnumerator<string> GetInputLinesEnumerator()
+	{
+		while (true) {
+			string[] splitedLineFromInput = streamReader.ReadLine().Split();
+			foreach (var line in splitedLineFromInput) {
+				yield return line;
+			}
+		}
+	}
+}
+
+internal class StreamWriterWrapper
+{
+	private readonly StreamWriter streamWriter;
+
+	public StreamWriterWrapper(StreamWriter streamWriter)
+	{
+		this.streamWriter = streamWriter;
+	}
+
+	public void Write(object obj) => streamWriter.Write(obj);
+
+	public void WriteLine(object obj) => streamWriter.WriteLine(obj);
+
+	public void WriteLine() => streamWriter.WriteLine();
+
+	public void Close()
+	{
+		streamWriter.Close();
+	}
+}
