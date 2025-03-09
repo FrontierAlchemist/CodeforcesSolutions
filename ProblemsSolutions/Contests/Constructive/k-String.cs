bd163a9f23a41a4280678097739960adcd101540
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-03-09 18:31:08 +0400

    Solve 'k-String' problem

---
+++ ProblemsSolutions/Contests/Constructive/k-String.cs
@@ -0,0 +1,191 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/219/A problem.
+/// </summary>
+internal class Solver
+{
+	private const bool IsSeveralTests = false;
+
+	private static StreamReaderWrapper Input => Program.Input;
+	private static StreamWriterWrapper Output => Program.Output;
+
+	public static void Run()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(Input.ReadString()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			Solve();
+		}
+	}
+
+	private static void Solve()
+	{
+		int parameterK = Input.ReadInt();
+		string sourceString = Input.ReadString();
+
+		Dictionary<char, int> lettersFrequencies = new();
+		for (int i = 0; i < sourceString.Length; ++i) {
+			if (!lettersFrequencies.ContainsKey(sourceString[i])) {
+				lettersFrequencies[sourceString[i]] = 0;
+			}
+			++lettersFrequencies[sourceString[i]];
+		}
+
+		bool isTargetStringExist = true;
+		foreach (int frequence in lettersFrequencies.Values) {
+			if (frequence % parameterK != 0) {
+				isTargetStringExist = false;
+				break;
+			}
+		}
+
+		if (isTargetStringExist) {
+			StringBuilder stringBuilder = new();
+			foreach ((char letter, int frequence) in lettersFrequencies) {
+				for (int i = 0; i < frequence / parameterK; ++i) {
+					stringBuilder.Append(letter);
+				}
+			}
+			string baseString = stringBuilder.ToString();
+			for (int i = 0; i < parameterK; ++i) {
+				Output.Write(baseString);
+			}
+			Output.WriteLine();
+		} else {
+			Output.WriteLine("-1");
+		}
+	}
+}
+
+internal class StreamReaderWrapper
+{
+	private readonly StreamReader streamReader;
+	private readonly IEnumerator<string> inputLinesEnumerator;
+
+	public StreamReaderWrapper(StreamReader streamReader)
+	{
+		this.streamReader = streamReader;
+		inputLinesEnumerator = GetInputLinesEnumerator();
+	}
+
+	public string ReadString() => ReadLine();
+
+	public char ReadChar() => ReadLine()[0];
+
+	public int ReadInt() => int.Parse(ReadLine());
+
+	public int[] ReadIntArray(int size)
+	{
+		int[] array = new int[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadInt();
+		}
+		return array;
+	}
+
+	public double ReadDouble() => double.Parse(ReadLine());
+
+	public double[] ReadDoubleArray(int size)
+	{
+		double[] array = new double[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadDouble();
+		}
+		return array;
+	}
+
+	public long ReadLong() => long.Parse(ReadLine());
+
+	public long[] ReadLongArray(int size)
+	{
+		long[] array = new long[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadLong();
+		}
+		return array;
+	}
+
+	public void Close()
+	{
+		streamReader.Close();
+	}
+
+	private string ReadLine()
+	{
+		inputLinesEnumerator.MoveNext();
+		return inputLinesEnumerator.Current;
+	}
+
+	private IEnumerator<string> GetInputLinesEnumerator()
+	{
+		while (true) {
+			string[] splitedLineFromInput = streamReader.ReadLine().Split();
+			foreach (var line in splitedLineFromInput) {
+				yield return line;
+			}
+		}
+	}
+}
+
+internal class StreamWriterWrapper
+{
+	private readonly StreamWriter streamWriter;
+
+	public StreamWriterWrapper(StreamWriter streamWriter)
+	{
+		this.streamWriter = streamWriter;
+	}
+
+	public void Write(object obj) => streamWriter.Write(obj);
+
+	public void WriteLine(object obj) => streamWriter.WriteLine(obj);
+
+	public void WriteLine() => streamWriter.WriteLine();
+
+	public void Close()
+	{
+		streamWriter.Close();
+	}
+}
+
+internal class Program
+{
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	public static StreamReaderWrapper Input { get; private set; }
+	public static StreamWriterWrapper Output { get; private set; }
+
+	private static void Main()
+	{
+		OpenIo();
+		Solver.Run();
+		CloseIo();
+	}
+
+	private static void OpenIo()
+	{
+		var inputStream = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		Input = new StreamReaderWrapper(inputStream);
+		var outputStream = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+		Output = new StreamWriterWrapper(outputStream);
+	}
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static void CloseIo()
+	{
+		Input.Close();
+		Output.Close();
+	}
+}
