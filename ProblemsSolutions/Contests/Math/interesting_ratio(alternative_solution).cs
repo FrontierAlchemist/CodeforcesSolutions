86d184949a62e4117beaea8d2217abd2629a4a33
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-03-26 14:34:11 +0400

    Merge branch 'CR-1013'

---
+++ ProblemsSolutions/Contests/Math/interesting_ratio(alternative_solution).cs
@@ -0,0 +1,196 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/contest/2091/problem/E problem.
+/// </summary>
+internal class Solver
+{
+	private const bool IsSeveralTests = true;
+
+	private static StreamReaderWrapper Input => Program.Input;
+	private static StreamWriterWrapper Output => Program.Output;
+
+	public static void Run()
+	{
+		DoPreprocessing();
+		int testsCount = IsSeveralTests ? int.Parse(Input.ReadString()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			Solve();
+		}
+	}
+
+	private static void DoPreprocessing()
+	{
+		MarkPrimeNumbers();
+	}
+
+	private const long MaxNumber = 10000000L;
+
+	private readonly static bool[] isPrimeNumber = new bool[MaxNumber + 1];
+
+	private static void MarkPrimeNumbers()
+	{
+		for (int i = 2; i <= MaxNumber; ++i) {
+			isPrimeNumber[i] = true;
+		}
+		isPrimeNumber[0] = false;
+		isPrimeNumber[1] = false;
+		for (long i = 2; i <= MaxNumber; ++i) {
+			long nextCompositeNumber = i * i;
+			if (nextCompositeNumber > MaxNumber) {
+				break;
+			}
+			if (!isPrimeNumber[i]) {
+				continue;
+			}
+			while (nextCompositeNumber <= MaxNumber) {
+				isPrimeNumber[nextCompositeNumber] = false;
+				nextCompositeNumber += i;
+			}
+		}
+	}
+
+	private static void Solve()
+	{
+		int n = Input.ReadInt();
+		long possiblePairsCount = 0;
+		for (int i = 2; i <= n; ++i) {
+			if (isPrimeNumber[i]) {
+				possiblePairsCount += n / i;
+			}
+		}
+		Output.WriteLine(possiblePairsCount);
+	}
+}
+
+internal class StreamReaderWrapper
+{
+	private readonly StreamReader streamReader;
+	private readonly IEnumerator<string> inputLinesEnumerator;
+
+	public StreamReaderWrapper(StreamReader streamReader)
+	{
+		this.streamReader = streamReader;
+		inputLinesEnumerator = GetInputLinesEnumerator();
+	}
+
+	public string ReadString() => ReadLine();
+
+	public char ReadChar() => ReadLine()[0];
+
+	public int ReadInt() => int.Parse(ReadLine());
+
+	public int[] ReadIntArray(int size)
+	{
+		int[] array = new int[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadInt();
+		}
+		return array;
+	}
+
+	public double ReadDouble() => double.Parse(ReadLine());
+
+	public double[] ReadDoubleArray(int size)
+	{
+		double[] array = new double[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadDouble();
+		}
+		return array;
+	}
+
+	public long ReadLong() => long.Parse(ReadLine());
+
+	public long[] ReadLongArray(int size)
+	{
+		long[] array = new long[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadLong();
+		}
+		return array;
+	}
+
+	public void Close()
+	{
+		streamReader.Close();
+	}
+
+	private string ReadLine()
+	{
+		inputLinesEnumerator.MoveNext();
+		return inputLinesEnumerator.Current;
+	}
+
+	private IEnumerator<string> GetInputLinesEnumerator()
+	{
+		while (true) {
+			string[] splitedLineFromInput = streamReader.ReadLine().Split();
+			foreach (var line in splitedLineFromInput) {
+				yield return line;
+			}
+		}
+	}
+}
+
+internal class StreamWriterWrapper
+{
+	private readonly StreamWriter streamWriter;
+
+	public StreamWriterWrapper(StreamWriter streamWriter)
+	{
+		this.streamWriter = streamWriter;
+	}
+
+	public void Write(object obj) => streamWriter.Write(obj);
+
+	public void WriteLine(object obj) => streamWriter.WriteLine(obj);
+
+	public void WriteLine() => streamWriter.WriteLine();
+
+	public void Close()
+	{
+		streamWriter.Close();
+	}
+}
+
+internal class Program
+{
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	public static StreamReaderWrapper Input { get; private set; }
+	public static StreamWriterWrapper Output { get; private set; }
+
+	private static void Main()
+	{
+		OpenIo();
+		Solver.Run();
+		CloseIo();
+	}
+
+	private static void OpenIo()
+	{
+		var inputStream = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		Input = new StreamReaderWrapper(inputStream);
+		var outputStream = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+		Output = new StreamWriterWrapper(outputStream);
+	}
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static void CloseIo()
+	{
+		Input.Close();
+		Output.Close();
+	}
+}
---
+++ ProblemsSolutions/Contests/Math/place_of_the_olympiad.cs
@@ -0,0 +1,176 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/contest/2091/problem/D problem.
+/// </summary>
+internal class Solver
+{
+	private const bool IsSeveralTests = true;
+
+	private static StreamReaderWrapper Input => Program.Input;
+	private static StreamWriterWrapper Output => Program.Output;
+
+	public static void Run()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(Input.ReadString()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			Solve();
+		}
+	}
+
+	private static void Solve()
+	{
+		int rowsCount = Input.ReadInt();
+		int columnsCount = Input.ReadInt();
+		int participantsCount = Input.ReadInt();
+
+		int minimalLengthOfLongestBench = 1;
+		int deskPerRow = Math.DivRem(participantsCount, rowsCount, out int remainDesks);
+		if (remainDesks > 0) {
+			++deskPerRow;
+		}
+		if (deskPerRow == columnsCount) {
+			minimalLengthOfLongestBench = deskPerRow;
+		} else {
+			int freeSpots = columnsCount - deskPerRow;
+			int benchesCount = freeSpots + 1;
+			minimalLengthOfLongestBench = Math.DivRem(deskPerRow, benchesCount, out int remainBenches);
+			if (remainBenches > 0) {
+				++minimalLengthOfLongestBench;
+			}
+		}
+		Output.WriteLine(minimalLengthOfLongestBench);
+	}
+}
+
+internal class StreamReaderWrapper
+{
+	private readonly StreamReader streamReader;
+	private readonly IEnumerator<string> inputLinesEnumerator;
+
+	public StreamReaderWrapper(StreamReader streamReader)
+	{
+		this.streamReader = streamReader;
+		inputLinesEnumerator = GetInputLinesEnumerator();
+	}
+
+	public string ReadString() => ReadLine();
+
+	public char ReadChar() => ReadLine()[0];
+
+	public int ReadInt() => int.Parse(ReadLine());
+
+	public int[] ReadIntArray(int size)
+	{
+		int[] array = new int[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadInt();
+		}
+		return array;
+	}
+
+	public double ReadDouble() => double.Parse(ReadLine());
+
+	public double[] ReadDoubleArray(int size)
+	{
+		double[] array = new double[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadDouble();
+		}
+		return array;
+	}
+
+	public long ReadLong() => long.Parse(ReadLine());
+
+	public long[] ReadLongArray(int size)
+	{
+		long[] array = new long[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadLong();
+		}
+		return array;
+	}
+
+	public void Close()
+	{
+		streamReader.Close();
+	}
+
+	private string ReadLine()
+	{
+		inputLinesEnumerator.MoveNext();
+		return inputLinesEnumerator.Current;
+	}
+
+	private IEnumerator<string> GetInputLinesEnumerator()
+	{
+		while (true) {
+			string[] splitedLineFromInput = streamReader.ReadLine().Split();
+			foreach (var line in splitedLineFromInput) {
+				yield return line;
+			}
+		}
+	}
+}
+
+internal class StreamWriterWrapper
+{
+	private readonly StreamWriter streamWriter;
+
+	public StreamWriterWrapper(StreamWriter streamWriter)
+	{
+		this.streamWriter = streamWriter;
+	}
+
+	public void Write(object obj) => streamWriter.Write(obj);
+
+	public void WriteLine(object obj) => streamWriter.WriteLine(obj);
+
+	public void WriteLine() => streamWriter.WriteLine();
+
+	public void Close()
+	{
+		streamWriter.Close();
+	}
+}
+
+internal class Program
+{
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	public static StreamReaderWrapper Input { get; private set; }
+	public static StreamWriterWrapper Output { get; private set; }
+
+	private static void Main()
+	{
+		OpenIo();
+		Solver.Run();
+		CloseIo();
+	}
+
+	private static void OpenIo()
+	{
+		var inputStream = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		Input = new StreamReaderWrapper(inputStream);
+		var outputStream = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+		Output = new StreamWriterWrapper(outputStream);
+	}
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static void CloseIo()
+	{
+		Input.Close();
+		Output.Close();
+	}
+}
