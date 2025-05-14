ca27702eb6fe75e676a4df58ea3fd89b6699ad72
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-05-14 13:03:37 +0400

    [Constructive] Solve 'Olya and Game with Arrays' problem

---
+++ ProblemsSolutions/Contests/Constructive/olya_and_game_with_arrays.cs
@@ -0,0 +1,206 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// Solve https://codeforces.com/problemset/problem/1859/B problem.
+/// </summary>
+internal class Solver
+{
+	private const bool IsSeveralTests = true;
+
+	private static StreamReaderWrapper Input => Program.Input;
+	private static StreamWriterWrapper Output => Program.Output;
+
+	public static void Run()
+	{
+		int testsCount = IsSeveralTests ? int.Parse(Input.ReadString()) : 1;
+		for (int i = 0; i < testsCount; ++i) {
+			Solve();
+		}
+	}
+
+	private static void Solve()
+	{
+		int arraysCount = Input.ReadInt();
+		int[] firstMinimalElements = new int[arraysCount];
+		int[] secondMinimalElements = new int[arraysCount];
+
+		CalculateMinimalsOfNextArray(0);
+		int mostMinimalFirstElement = firstMinimalElements[0];
+		int mostMinimalSecondElement = secondMinimalElements[0];
+
+		for (int i = 1; i < arraysCount; ++i) {
+			CalculateMinimalsOfNextArray(i);
+			if (firstMinimalElements[i] < mostMinimalFirstElement) {
+				mostMinimalFirstElement = firstMinimalElements[i];
+			}
+			if (secondMinimalElements[i] < mostMinimalSecondElement) {
+				mostMinimalSecondElement = secondMinimalElements[i];
+			}
+		}
+
+		long maximalBeautyOfArrays = 0L;
+		for (int i = 0; i < arraysCount; ++i) {
+			maximalBeautyOfArrays += secondMinimalElements[i];
+		}
+
+		maximalBeautyOfArrays -= mostMinimalSecondElement;
+		maximalBeautyOfArrays += mostMinimalFirstElement;
+
+		Output.WriteLine(maximalBeautyOfArrays);
+
+		void CalculateMinimalsOfNextArray(int arrayIndex)
+		{
+			int arraySize = Input.ReadInt();
+
+			int firstMinimum = Input.ReadInt();
+			int secondMinimum = Input.ReadInt();
+			if (secondMinimum < firstMinimum) {
+				(firstMinimum, secondMinimum) = (secondMinimum, firstMinimum);
+			}
+			for (int i = 2; i < arraySize; ++i) {
+				int arrayElement = Input.ReadInt();
+				if (arrayElement <= firstMinimum) {
+					secondMinimum = firstMinimum;
+					firstMinimum = arrayElement;
+				} else if (arrayElement < secondMinimum) {
+					secondMinimum = arrayElement;
+				}
+			}
+
+			firstMinimalElements[arrayIndex] = firstMinimum;
+			secondMinimalElements[arrayIndex] = secondMinimum;
+		}
+	}
+}
+
+internal class StreamReaderWrapper
+{
+	private readonly StreamReader streamReader;
+	private readonly IEnumerator<string> inputLinesEnumerator;
+
+	public StreamReaderWrapper(StreamReader streamReader)
+	{
+		this.streamReader = streamReader;
+		inputLinesEnumerator = GetInputLinesEnumerator();
+	}
+
+	public string ReadString() => ReadLine();
+
+	public char ReadChar() => ReadLine()[0];
+
+	public int ReadInt() => int.Parse(ReadLine());
+
+	public int[] ReadIntArray(int size)
+	{
+		int[] array = new int[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadInt();
+		}
+		return array;
+	}
+
+	public double ReadDouble() => double.Parse(ReadLine());
+
+	public double[] ReadDoubleArray(int size)
+	{
+		double[] array = new double[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadDouble();
+		}
+		return array;
+	}
+
+	public long ReadLong() => long.Parse(ReadLine());
+
+	public long[] ReadLongArray(int size)
+	{
+		long[] array = new long[size];
+		for (int i = 0; i < size; ++i) {
+			array[i] = ReadLong();
+		}
+		return array;
+	}
+
+	public void Close()
+	{
+		streamReader.Close();
+	}
+
+	private string ReadLine()
+	{
+		inputLinesEnumerator.MoveNext();
+		return inputLinesEnumerator.Current;
+	}
+
+	private IEnumerator<string> GetInputLinesEnumerator()
+	{
+		while (true) {
+			string[] splitedLineFromInput = streamReader.ReadLine().Split();
+			foreach (var line in splitedLineFromInput) {
+				yield return line;
+			}
+		}
+	}
+}
+
+internal class StreamWriterWrapper
+{
+	private readonly StreamWriter streamWriter;
+
+	public StreamWriterWrapper(StreamWriter streamWriter)
+	{
+		this.streamWriter = streamWriter;
+	}
+
+	public void Write(object obj) => streamWriter.Write(obj);
+
+	public void WriteLine(object obj) => streamWriter.WriteLine(obj);
+
+	public void WriteLine() => streamWriter.WriteLine();
+
+	public void Close()
+	{
+		streamWriter.Close();
+	}
+}
+
+internal class Program
+{
+	private const string InputFilePath = "..\\..\\..\\input.txt";
+	private const string OutputFilePath = "..\\..\\..\\output.txt";
+
+	public static StreamReaderWrapper Input { get; private set; }
+	public static StreamWriterWrapper Output { get; private set; }
+
+	private static void Main()
+	{
+		OpenIo();
+		Solver.Run();
+		CloseIo();
+	}
+
+	private static void OpenIo()
+	{
+		var inputStream = IsDebug() ? new StreamReader(InputFilePath) : new StreamReader(Console.OpenStandardInput());
+		Input = new StreamReaderWrapper(inputStream);
+		var outputStream = IsDebug() ? new StreamWriter(OutputFilePath) : new StreamWriter(Console.OpenStandardOutput());
+		Output = new StreamWriterWrapper(outputStream);
+	}
+
+	private static bool IsDebug()
+	{
+#if DEBUG
+		return true;
+#else
+		return false;
+#endif
+	}
+
+	private static void CloseIo()
+	{
+		Input.Close();
+		Output.Close();
+	}
+}
