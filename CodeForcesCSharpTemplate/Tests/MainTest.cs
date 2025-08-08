aec59613c49274180ec9e9f1c410747b198bdbf9
Anton Podchuvalov <aa.podchuvalov@gmail.com>
2025-08-08 23:16:04 +0400

    Add UnitTests project into CSharpTemplate solution

---
+++ CodeForcesCSharpTemplate/Tests/MainTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Xunit;
+using CodeForces;
+
+namespace Tests;
+
+public class MainTest
+{
+	[Fact]
+	public void Test()
+	{
+		Assert.Throws<NullReferenceException>(Solver.Run);
+	}
+}
